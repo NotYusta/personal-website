79be9e9b996debba38ba3deba3435fa8eb0e357b
RedEpic <abhistanolimit@gmail.com>
2021-02-20 17:54:16 +0700

    Added Home Page Title

---
+++ index.php
@@ -0,0 +1,7 @@
+<head>
+    <title>Home Page - xAbhista</title>
+    <link rel="icon" href="https://avatars.githubusercontent.com/u/53965703?s=460&u=abe389eed07bb35d3b668ca2647be9047549ac9a&v=4"/>
+    <meta name="og:title" content="Home Page - xAbhista"/>
+    <meta name="og:image" content="https://avatars.githubusercontent.com/u/53965703?s=460&u=abe389eed07bb35d3b668ca2647be9047549ac9a&v=4"/>
+    <link rel="stylesheet" href="assets/css/style.css"/>
+</head>
